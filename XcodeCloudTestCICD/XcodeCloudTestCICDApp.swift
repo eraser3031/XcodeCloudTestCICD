b12e89e94e5f1aa9d076e98cf39d2895f7235af7
eraser <eraser3031@gmail.com>
2022-11-18 11:41:39 +0900

    Initial Commit

---
+++ XcodeCloudTestCICD/XcodeCloudTestCICDApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeCloudTestCICDApp.swift
+//  XcodeCloudTestCICD
+//
+//  Created by 김예훈 on 2022/11/18.
+//
+
+import SwiftUI
+
+@main
+struct XcodeCloudTestCICDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
